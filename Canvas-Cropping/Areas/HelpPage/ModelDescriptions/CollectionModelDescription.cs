aff79c0ed4fafb950b434f5f52a7f3598317973f
Gal Attila <radeon16@hotmail.com>
2018-07-09 00:02:55 +0300

    - initial commit

---
+++ Canvas-Cropping/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Canvas_Cropping.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Canvas-Cropping/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Canvas_Cropping.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
